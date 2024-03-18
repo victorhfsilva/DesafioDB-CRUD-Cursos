2cf6ac952fccebb5e3e61d102a77faf8bf929ae4
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-03-18 17:27:04 -0300

    feat: Definição dos repositórios de alunos, professores, contatos e endereços.

---
+++ crud-cursos-back-end/src/main/java/com/db/crudcursosbackend/domain/usuario/aluno/repositorios/AlunoRepository.java
@@ -0,0 +1,62 @@
+package com.db.crudcursosbackend.domain.usuario.aluno.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.db.crudcursosbackend.domain.usuario.aluno.Aluno;
+import com.db.crudcursosbackend.domain.usuario.estado.Estado;
+
+@RepositoryRestResource(path="alunos")
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    
+    @RestResource(exported = false)
+    @Override
+    <S extends Aluno> S save(S entity);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteById(Long id);
+
+    @RestResource(exported = false)
+    @Override
+    void delete(Aluno entity);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteAll(Iterable<? extends Aluno> entities);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteAll();
+    
+    Optional<Aluno> findByCpf(String cpf);
+
+    Optional<Aluno> findByMatricula(String matricula);
+
+    @Query("SELECT a FROM Aluno a WHERE a.contato.email = :email")
+    Optional<Aluno> findByEmail(String email);
+
+    @Query("SELECT a FROM Aluno a WHERE a.contato.celular = :celular")
+    Optional<Aluno> findByCelular(String celular);
+
+    @Query("SELECT a FROM Aluno a JOIN a.enderecos e WHERE e.cidade = :cidade")
+    List<Aluno> findByCidade(@Param("cidade") String cidade);
+
+    @Query("SELECT a FROM Aluno a JOIN a.enderecos e WHERE e.rua = :rua")
+    List<Aluno> findByRua(@Param("rua") String rua);
+
+    @Query("SELECT a FROM Aluno a JOIN a.enderecos e WHERE e.bairro = :bairro")
+    List<Aluno> findByBairro(@Param("bairro") String bairro);
+
+    @Query("SELECT a FROM Aluno a JOIN a.enderecos e WHERE e.cep = :cep")
+    List<Aluno> findByCep(@Param("cep") String cep);
+
+    @Query("SELECT a FROM Aluno a JOIN a.enderecos e WHERE e.estado = :estado")
+    List<Aluno> findByEstado(@Param("estado") Estado estado);
+}
---
+++ crud-cursos-back-end/src/main/java/com/db/crudcursosbackend/domain/usuario/professor/repositorios/ProfessorRepository.java
@@ -0,0 +1,58 @@
+package com.db.crudcursosbackend.domain.usuario.professor.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import com.db.crudcursosbackend.domain.usuario.professor.Professor;
+import com.db.crudcursosbackend.domain.usuario.estado.Estado;
+
+@RepositoryRestResource(path="alunos")
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    
+    @RestResource(exported = false)
+    @Override
+    <S extends Professor> S save(S entity);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteById(Long id);
+
+    @RestResource(exported = false)
+    @Override
+    void delete(Professor entity);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteAll(Iterable<? extends Professor> entities);
+
+    @RestResource(exported = false)
+    @Override
+    void deleteAll();
+    
+    Optional<Professor> findByCpf(String cpf);
+
+    @Query("SELECT p FROM Professor p WHERE p.contato.email = :email")
+    Optional<Professor> findByEmail(String email);
+
+    @Query("SELECT p FROM Professor p WHERE p.contato.celular = :celular")
+    Optional<Professor> findByCelular(String celular);
+
+    @Query("SELECT p FROM Professor p JOIN p.enderecos e WHERE e.cidade = :cidade")
+    List<Professor> findByCidade(@Param("cidade") String cidade);
+
+    @Query("SELECT p FROM Professor p JOIN p.enderecos e WHERE e.rua = :rua")
+    List<Professor> findByRua(@Param("rua") String rua);
+
+    @Query("SELECT p FROM Professor p JOIN p.enderecos e WHERE e.bairro = :bairro")
+    List<Professor> findByBairro(@Param("bairro") String bairro);
+
+    @Query("SELECT p FROM Professor p JOIN p.enderecos e WHERE e.cep = :cep")
+    List<Professor> findByCep(@Param("cep") String cep);
+
+    @Query("SELECT p FROM Professor p JOIN p.enderecos e WHERE e.estado = :estado")
+    List<Professor> findByEstado(@Param("estado") Estado estado);
+}
