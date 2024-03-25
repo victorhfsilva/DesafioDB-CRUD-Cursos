62f751fa8987a20489f2212fa64404863d4169e0
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-03-25 11:12:00 -0300

    test: Testes dos controladores de registro, adicionar e remover alunos de cursos.

--- crud-cursos-back-end/src/test/java/com/db/crudcursosbackend/controller/cursos/CursoAdminControllerTest.java
+++ crud-cursos-back-end/src/test/java/com/db/crudcursosbackend/controller/cursos/CursoAdminControllerTest.java
@@ -1,5 +1,6 @@
 package com.db.crudcursosbackend.controller.cursos;
 
+import java.time.LocalDate;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,13 +20,21 @@ import com.db.crudcursosbackend.domain.cursos.Curso;
 import com.db.crudcursosbackend.domain.cursos.CursoBuilder;
 import com.db.crudcursosbackend.domain.cursos.dtos.CursoDTO;
 import com.db.crudcursosbackend.domain.cursos.servicos.CursoService;
+import com.db.crudcursosbackend.domain.usuario.aluno.Aluno;
+import com.db.crudcursosbackend.domain.usuario.aluno.AlunoBuilder;
+import com.db.crudcursosbackend.domain.usuario.aluno.dtos.AlunoDTO;
 import com.db.crudcursosbackend.domain.usuario.contato.Contato;
 import com.db.crudcursosbackend.domain.usuario.contato.ContatoBuilder;
+import com.db.crudcursosbackend.domain.usuario.endereco.Endereco;
+import com.db.crudcursosbackend.domain.usuario.endereco.EnderecoBuilder;
+import com.db.crudcursosbackend.domain.usuario.endereco.dtos.EnderecoDTO;
+import com.db.crudcursosbackend.domain.usuario.estado.Estado;
 import com.db.crudcursosbackend.domain.usuario.pessoa.servicos.PessoaService;
 import com.db.crudcursosbackend.domain.usuario.professor.Professor;
 import com.db.crudcursosbackend.domain.usuario.professor.ProfessorBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -181,7 +190,7 @@ class CursoAdminControllerTest {
 
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
-    void dadoUmCurso_quandoAtualizadProfessoro_deveRetornarProfessorAtualizados() throws Exception{
+    void dadoUmCurso_quandoAtualizadoProfessor_deveRetornarProfessorAtualizados() throws Exception{
     
         ProfessorBuilder professorBuilder = new ProfessorBuilder();
         ContatoBuilder contatoBuilder = new ContatoBuilder();
@@ -225,4 +234,183 @@ class CursoAdminControllerTest {
                                                 .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("professor"));
     }
 
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void dadoUmCurso_quandoAlunoRemovido_deveRetornarCursoCorretamente() throws Exception{
+    
+        ProfessorBuilder professorBuilder = new ProfessorBuilder();
+        ContatoBuilder contatoBuilder = new ContatoBuilder();
+        EnderecoBuilder enderecoBuilder = new EnderecoBuilder();
+        
+        Contato contatoProfessor = contatoBuilder.celular("1234453789")
+                                                .email("professor@db.com")
+                                                .build();
+
+        Endereco endereco1 = enderecoBuilder.numero("126")
+                                                .complemento("Ap. 201")
+                                                .rua("Rua")
+                                                .bairro("Bairro")
+                                                .cidade("Salvador")
+                                                .estado(Estado.BAHIA)
+                                                .cep("1234567")
+                                                .build();
+
+        Endereco endereco2 = enderecoBuilder.numero("126")
+                                                .complemento("Ap. 204")
+                                                .rua("Rua")
+                                                .bairro("Bairro")
+                                                .cidade("Salvador")
+                                                .estado(Estado.BAHIA)
+                                                .cep("1234567")
+                                                .build();
+
+        List<Endereco> enderecos = List.of(endereco1, endereco2);
+
+        Professor professor  = professorBuilder.ativo(true)
+                                        .cpf("64174892563")
+                                        .nome("professor")
+                                        .senha("senha123")
+                                        .contato(contatoProfessor)
+                                        .enderecos(enderecos)
+                                        .build();
+
+        CursoBuilder cursoBuilder = new CursoBuilder();
+
+        Curso curso = cursoBuilder.nome("Matemática") 
+                                            .descricao("Matemática para todos")
+                                            .cargaHoraria(300)
+                                            .professor(professor)
+                                            .alunos(List.of())
+                                            .build();
+
+        when(cursoService.removerAluno(1L, "64174892563")).thenReturn(curso);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/admin/curso/atualizar/1/remover/aluno/64174892563"))
+                                                .andExpect(MockMvcResultMatchers.status().isOk())
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Matemática"))
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.cargaHoraria").value("300"));
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void dadoUmCurso_quandoAlunoAdicionado_deveRetornarCursoCorretamente() throws Exception{
+    
+        ProfessorBuilder professorBuilder = new ProfessorBuilder();
+        ContatoBuilder contatoBuilder = new ContatoBuilder();
+        EnderecoBuilder enderecoBuilder = new EnderecoBuilder();
+        AlunoBuilder alunoBuilder = new AlunoBuilder();
+        
+        Contato contatoProfessor = contatoBuilder.celular("1234453789")
+                                                .email("professor@db.com")
+                                                .build();
+
+        Endereco endereco1 = enderecoBuilder.numero("126")
+                                                .complemento("Ap. 201")
+                                                .rua("Rua")
+                                                .bairro("Bairro")
+                                                .cidade("Salvador")
+                                                .estado(Estado.BAHIA)
+                                                .cep("1234567")
+                                                .build();
+
+        Endereco endereco2 = enderecoBuilder.reset().numero("126")
+                                                .complemento("Ap. 204")
+                                                .rua("Rua")
+                                                .bairro("Bairro")
+                                                .cidade("Salvador")
+                                                .estado(Estado.BAHIA)
+                                                .cep("1234567")
+                                                .build();
+
+        List<Endereco> enderecosProfessor = List.of(endereco1, endereco2);
+
+        Professor professor  = professorBuilder.ativo(true)
+                                        .cpf("64174892563")
+                                        .nome("professor")
+                                        .senha("senha123")
+                                        .contato(contatoProfessor)
+                                        .enderecos(enderecosProfessor)
+                                        .build();
+
+
+        Contato contatoAluno = contatoBuilder.celular("12342133789")
+                                            .email("aluno@db.com")
+                                            .build();
+
+        Endereco endereco3 = enderecoBuilder.reset().numero("126")
+                                                    .complemento("Ap. 204")
+                                                    .rua("Rua")
+                                                    .bairro("Bairro")
+                                                    .cidade("Salvador")
+                                                    .estado(Estado.BAHIA)
+                                                    .cep("1234567")
+                                                    .build();
+
+        List<Endereco> enderecosAluno = List.of(endereco3);
+
+        Aluno aluno = alunoBuilder.nome("Nome")
+                                                .sobrenome("Sobrenome")
+                                                .cpf("73565638435")
+                                                .senha("L33tP@swd")
+                                                .dataDeNascimento(LocalDate.of(2004, 3, 7))
+                                                .contato(contatoAluno)
+                                                .enderecos(enderecosAluno)
+                                                .build();
+
+        CursoBuilder cursoBuilder = new CursoBuilder();
+
+        Curso curso = cursoBuilder.nome("Matemática") 
+                                            .descricao("Matemática para todos")
+                                            .cargaHoraria(300)
+                                            .professor(professor)
+                                            .alunos(List.of(aluno))
+                                            .build();
+
+        when(cursoService.adicionarAluno(1L, "64174892563")).thenReturn(curso);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/admin/curso/atualizar/1/adicionar/aluno/64174892563"))
+                                                .andExpect(MockMvcResultMatchers.status().isOk())
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Matemática"))
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.cargaHoraria").value("300"));
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void dadoUmCurso_quandoRegistrado_deveRetornarDadosAtualizados() throws Exception{
+    
+        ProfessorBuilder professorBuilder = new ProfessorBuilder();
+        ContatoBuilder contatoBuilder = new ContatoBuilder();
+
+        Contato contatoEditor = contatoBuilder.celular("123456789")
+                                                .email("admin@db.com")
+                                                .build();
+
+        Professor editor  = professorBuilder.ativo(true)
+                                        .cpf("admin")
+                                        .nome("admin")
+                                        .senha("admin")
+                                        .contato(contatoEditor)
+                                        .build();
+
+        when(pessoaService.buscarPorCpf("admin")).thenReturn(editor);
+
+        CursoDTO cursoDTO = CursoDTO.builder().nome("Matemática") 
+                                                .descricao("Matemática para todos")
+                                                .cargaHoraria(300)
+                                                .build();
+
+        String cursoJson = objectMapper.writeValueAsString(cursoDTO);
+        Curso curso = cursoDTO.converterParaEntidade();
+
+        when(cursoService.registrar(curso,  "73565638435", editor)).thenReturn(curso);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/admin/curso/registrar/73565638435")
+                                                .contentType("application/json")
+                                                .content(cursoJson))
+                                                .andExpect(MockMvcResultMatchers.status().isCreated())
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Matemática"))
+                                                .andExpect(MockMvcResultMatchers.jsonPath("$.cargaHoraria").value("300"));
+    }
 }
